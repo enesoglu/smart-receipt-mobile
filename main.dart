b906c55be0510b1c94956c8e33737b202505c7e5
Enes Yıldız <yildiz_enes@hotmail.com>
2025-10-12 21:58:14 +0300

    initial commit

---
+++ main.dart
@@ -0,0 +1,27 @@
+import 'package:flutter/material.dart';
+
+void main() => runApp(MyApp());
+
+class MyApp extends StatelessWidget {
+  const MyApp({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: 'Flutter Demo',
+      theme: ThemeData(primarySwatch: Colors.cyan),
+      home: Scaffold(
+        backgroundColor: Colors.cyan[50],
+        appBar: AppBar(title: Center(child: Text("Hello World"))),
+        floatingActionButton: FloatingActionButton(
+          onPressed: printHelloWorld,
+          child: Icon(Icons.print),
+        ),
+      ),
+    );
+  }
+}
+
+void printHelloWorld() {
+  print("Hello World!");
+}
